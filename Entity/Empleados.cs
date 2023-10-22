691cc7c98fd2fb1eb48436b2ec381dbfe84fa383
GermanAndresLopez <gandreslopez@unicesar.edu.co>
2023-10-21 19:09:35 -0500

    Agregar archivos de proyecto.

---
+++ Entity/Empleados.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Empleados
+    {
+        public Empleados()
+        {
+            salarioBase = 0;
+        }
+        public string identifiacion {  get; set; }
+        public string nombre { get; set; }
+        public double salarioBase { get; set; }
+        public string estado { get; set; }
+
+    }
+}
